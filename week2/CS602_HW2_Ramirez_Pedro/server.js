afb880a1ce96f195e38237f02ec7d71acb962824
Pedro Ramirez <prlara2000@gmail.com>
2025-01-26 21:02:59 -0600

    finalize

--- week2/CS602_HW2_Ramirez_Pedro/server.js
+++ week2/CS602_HW2_Ramirez_Pedro/server.js
@@ -22,28 +22,28 @@ import * as courseDB from './courseModule.js';
 // GET request to the homepage
 
 app.get('/', (req, res) => {
-  res.render('homeView');
+  res.status(200).render('homeView');
 });
 
 app.get('/random', (req, res) => {
   const result = courseDB.getRandomCourse();
-  res.render('randomView', result);
+  res.status(200).render('randomView', result);
 });
 
 app.get('/cid', (req, res) => {
   if (req.query.id) {
     let id = req.query.id;
     let result = courseDB.lookupByCourseId(id);
-    res.render('lookupByCourseIdView', { query: id, courses: result });
+    res.status(200).render('lookupByCourseIdView', { query: id, courses: result });
   } else {
-    res.render('lookupByCourseIdForm');
+    res.status(200).render('lookupByCourseIdForm');
   }
 });
 
 app.post('/cid', (req, res) => {
   let id = req.body.id;
   let result = courseDB.lookupByCourseId(id);
-  res.render('lookupByCourseIdView', { query: id, courses: result });
+  res.status(200).render('lookupByCourseIdView', { query: id, courses: result });
 });
 
 app.get('/cid/:id', (req, res) => {
@@ -84,22 +84,29 @@ app.get('/cid/:id', (req, res) => {
 });
 
 app.get('/cname', (req, res) => {
-  res.render('lookupByCourseNameForm');
+  if (req.query.name) {
+    let name = req.query.name;
+    let result = courseDB.lookupByCourseName(name);
+    res.status(200).render('lookupByCourseNameView', { query: name, courses: result });
+  } else {
+    res.status(200).render('lookupByCourseNameForm');
+  }
 });
 
 app.post('/cname', (req, res) => {
   let name = req.body.name;
   let result = courseDB.lookupByCourseName(name);
-  res.render('lookupByCourseNameView', { query: name, courses: result });
+  res.status(200).render('lookupByCourseNameView', { query: name, courses: result });
 });
 
 app.get('/cname/:name', (req, res) => {
   let name = req.params.name;
+  console.log('name', name);
   let result = courseDB.lookupByCourseName(name);
 
   res.format({
     'application/json': () => {
-      res.json({ query: name, courses: result });
+      res.status(200).json({ query: name, courses: result });
     },
 
     'application/xml': () => {
@@ -111,17 +118,17 @@ app.get('/cname/:name', (req, res) => {
           })
           .join('\n') +
         `\n</courses>`;
-      res.type('application/xml').send(coursesXml);
+      res.status(200).type('application/xml').send(coursesXml);
     },
 
     'text/html': () => {
-      res.render('lookupByCourseNameView', { query: name, courses: result });
+      res.status(200).render('lookupByCourseNameView', { query: name, courses: result });
     },
 
     'text/plain': () => {
       let coursesText =
         `Query: ${name}\n` + result.map((course) => `${course.course_id}: ${course.course_name}`).join('\n');
-      res.type('text/plain').send(coursesText);
+      res.status(200).type('text/plain').send(coursesText);
     },
 
     default: () => {
