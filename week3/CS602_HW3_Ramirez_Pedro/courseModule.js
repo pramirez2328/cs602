237739e0cf3bf806ba7199b209cafdac66239c59
Pedro Ramirez <prlara2000@gmail.com>
2025-01-29 22:36:53 -0600

    feat: finish part 1

--- week3/CS602_HW3_Ramirez_Pedro/courseModule.js
+++ week3/CS602_HW3_Ramirez_Pedro/courseModule.js
@@ -2,51 +2,41 @@ import Sequelize from 'sequelize';
 
 import { Op } from 'sequelize';
 
-import { db }  from './db.js';
+import { db } from './db.js';
 
-import { Course, Coordinator } from
-   './models/OneToManyModel.js';
+import { Course, Coordinator } from './models/OneToManyModel.js';
 
-export const lookupByCourseId =  async (id) => {
-	console.log("\nLookup by CourseId:", id);
-	let result;
+export const lookupByCourseId = async (id) => {
+  console.log('\nLookup by CourseId:', id);
+  const result = await Course.findAll({ where: { courseId: { [Op.like]: id } }, include: Coordinator });
 
-	// Fill in the code
-
-	return JSON.parse(JSON.stringify(result));
+  return JSON.parse(JSON.stringify(result));
 };
 
 export const lookupByCourseName = async (name) => {
-  console.log("\nLookup by CourseName:", name);
-	let result ;
+  console.log('\nLookup by CourseName:', name);
+  const result = await Course.findAll({ where: { courseName: { [Op.like]: name } }, include: Coordinator });
 
-	// Fill in the code
-
-	return JSON.parse(JSON.stringify(result));
+  return JSON.parse(JSON.stringify(result));
 };
 
-export const lookupByCoordinator =  async (id) => {
-	console.log("\nLookup by Coordinator:", id);
-	let result ;
+export const lookupByCoordinator = async (id) => {
+  console.log('\nLookup by Coordinator:', id);
+  const result = await Coordinator.findByPk(id, { include: Course });
 
-	// Fill in the code
-
-	return JSON.parse(JSON.stringify(result));
+  return JSON.parse(JSON.stringify(result));
 };
 
 export const getRandomCourse = async () => {
-	console.log("\nA Random Course:");
-	let result = await Course.findOne(
-		{ order:  Sequelize.fn('RANDOM'),
-		include: Coordinator });
+  console.log('\nA Random Course:');
+  let result = await Course.findOne({ order: Sequelize.fn('RANDOM'), include: Coordinator });
 
-	return JSON.parse(JSON.stringify(result));
+  return JSON.parse(JSON.stringify(result));
 };
 
 export const getCourseDescription = async (id) => {
-	console.log("\nGet Course Description:", id);
-	let response = await fetch("https://kalathur.com/php/getCourseData.php?id=cs" + id);
-	let result = await response.json();
-	return result;
-}
-
+  console.log('\nGet Course Description:', id);
+  let response = await fetch('https://kalathur.com/php/getCourseData.php?id=cs' + id);
+  let result = await response.json();
+  return result;
+};
--- week3/CS602_HW3_Ramirez_Pedro/testCourseModule.js
+++ week3/CS602_HW3_Ramirez_Pedro/testCourseModule.js
@@ -2,20 +2,53 @@ import * as courseDB from './courseModule.js';
 
 let result;
 
-result = await courseDB.lookupByCourseId("CS %77");
+result = await courseDB.lookupByCourseId('CS %77');
 console.log(result);
+console.log('\n-----------------------------------');
 
-result = await courseDB.lookupByCourseName("%Python%");
+result = await courseDB.lookupByCourseName('%Python%');
 console.log(result);
+console.log('\n-----------------------------------');
 
 result = await courseDB.lookupByCoordinator('kalathur');
 console.log(result);
+console.log('\n-----------------------------------');
 
 result = await courseDB.getRandomCourse();
 console.log(result);
+console.log('\n-----------------------------------');
 
 result = await courseDB.getCourseDescription('632');
 console.log(result);
+console.log('\n-----------------------------------');
 
-// Write One additional test case for each of the above.
-// Make sure lookupByCourseId and lookupByCourseName provide more than one match.
+// two extra tests for each function
+
+result = await courseDB.lookupByCourseId('%01');
+console.log(result);
+result = await courseDB.lookupByCourseId('CS 555');
+console.log(result);
+console.log('\n-----------------------------------');
+
+result = await courseDB.lookupByCourseName('Database%');
+console.log(result);
+result = await courseDB.lookupByCourseName('Cyber%');
+console.log(result);
+console.log('\n-----------------------------------');
+
+result = await courseDB.lookupByCoordinator('day');
+console.log(result);
+result = await courseDB.lookupByCoordinator('temkin');
+console.log(result);
+console.log('\n-----------------------------------');
+
+result = await courseDB.getRandomCourse();
+console.log(result);
+result = await courseDB.getRandomCourse();
+console.log(result);
+console.log('\n-----------------------------------');
+
+result = await courseDB.getCourseDescription('341');
+console.log(result);
+result = await courseDB.getCourseDescription('521');
+console.log(result);
