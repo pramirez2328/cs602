c8022b57579a89347a0089187f47d7f85ccc0858
Pedro Ramirez <prlara2000@gmail.com>
2025-02-14 22:53:12 -0600

    feat: add two more test

--- week5/CS602_HW5_Ramirez_Pedro/2_Client/graphQL_client.js
+++ week5/CS602_HW5_Ramirez_Pedro/2_Client/graphQL_client.js
@@ -1,32 +1,61 @@
 // File: ex02_graphQL_client.js
 
-import * as clientModule from './clientCourseModule.js'
+import * as clientModule from './clientCourseModule.js';
 
 let result;
 
-
-console.log("\nQuery random course");
+console.log('\nQuery random course');
 result = await clientModule.getRandomCourse();
 console.log(JSON.stringify(result, null, 2));
 
-console.log("\nQuery course description");
-result = await clientModule.getCourseDescription("602");
+console.log('\nQuery course description');
+result = await clientModule.getCourseDescription('602');
 console.log(JSON.stringify(result, null, 2));
 
-console.log("\nQuery courses by id - Basic Info");
-result = await clientModule.lookupByCourseId_V1("CS 6.2");
+console.log('\nQuery courses by id - Basic Info');
+result = await clientModule.lookupByCourseId_V1('CS 6.2');
 console.log(JSON.stringify(result, null, 2));
 
-console.log("\nQuery courses by id - with Coordinator and associated courses");
-result = await clientModule.lookupByCourseId_V2("CS 6.2");
+console.log('\nQuery courses by id - with Coordinator and associated courses');
+result = await clientModule.lookupByCourseId_V2('CS 6.2');
 console.log(JSON.stringify(result, null, 2));
 
-console.log("\nQuery courses by name ");
-result = await clientModule.lookupByCourseName("^database");
+console.log('\nQuery courses by name ');
+result = await clientModule.lookupByCourseName('^database');
 console.log(JSON.stringify(result, null, 2));
 
-console.log("\nQuery coordinator by id - with associated courses");
-result = await clientModule.lookupByCoordinator("kalathur");
+console.log('\nQuery coordinator by id - with associated courses');
+result = await clientModule.lookupByCoordinator('kalathur');
 console.log(JSON.stringify(result, null, 2));
+
+console.log('\n--------------------------------------------------------------------');
+
+console.log('\nOne extra query of each type');
+
+console.log('\nQuery random course');
+result = await clientModule.getRandomCourse();
+console.log(JSON.stringify(result, null, 2));
+
+console.log('\nQuery course description');
+result = await clientModule.getCourseDescription('777');
+console.log(JSON.stringify(result, null, 2));
+
+console.log('\nQuery courses by id - Basic Info');
+result = await clientModule.lookupByCourseId_V1('CS 7.7');
+console.log(JSON.stringify(result, null, 2));
+
+console.log('\nQuery courses by id - with Coordinator and associated courses');
+result = await clientModule.lookupByCourseId_V2('CS 7.7');
+console.log(JSON.stringify(result, null, 2));
+
+console.log('\nQuery courses by name ');
+result = await clientModule.lookupByCourseName('^Web');
+console.log(JSON.stringify(result, null, 2));
+
+console.log('\nQuery coordinator by id - with associated courses');
+result = await clientModule.lookupByCoordinator('day');
+console.log(JSON.stringify(result, null, 2));
+
+console.log('\n--------------------------------------------------------------------');
 
 clientModule.client.clearStore();
